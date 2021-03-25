526b504a6991c231439bc0f87a89d623780c64e3
a18filkr <a18filkr@student.his.se>
2021-03-25 14:10:55 +0100

    New folder

---
+++ pilot-nativescript/Pilot-NativeScript/src/components/App.vue
@@ -0,0 +1,62 @@
+<template>
+    <Page>
+        <ActionBar title="NativeScript Pilot-App"/>
+        <StackLayout>
+            <Label class="message" :text="msg2" height="auto" textWrap="true"/>
+            <Label class="message" :text="msg" height="auto" textWrap="true"/>
+            <Button @tap="start" :isEnabled="!isStarted" text="Starta timer" />
+            <Clicker v-if="isStarted" :delay="delay" @reactionTime="result"></Clicker>
+            <Result v-if="showResult" :time="time"></Result>
+        </StackLayout>
+    </Page>
+</template>
+
+<script >
+  import Clicker from './Clicker.vue'
+  import Result from './Result.vue'
+  export default {
+    name: 'App',
+    components: { Clicker, Result },
+    data(){
+      return {
+        isStarted: false,
+        delay: null,
+        time: null,
+        showResult: true,
+        msg: 'Genom att klicka på knappen startas en timer mellan 3-6 sekunder som ska klickas på så fort som möjligt',
+        msg2: 'Liten testapp för att se vad som skiljer i NativeScript och Ionic utan att använda någon av mobilens funktioner'
+      }
+    },
+  methods: {
+    start() {
+      this.delay = Math.floor(3000 + Math.random() * 3000)
+      this.isStarted = true
+      this.showResult = false;
+      console.log(this.isStarted, this.delay, this.showResult);
+    },
+    result(time) {
+      this.time = time
+      this.isStarted = false
+      this.showResult = true
+      console.log("This is the time:", this.time)
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+    ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+
+    .message {
+        vertical-align: center;
+        text-align: center;
+        font-size: 16;
+        color: #333333;
+        margin: 15 0;
+        padding: 5;
+    }
+</style>
---
+++ pilot-nativescript/Pilot-NativeScript/src/components/Result.vue
@@ -0,0 +1,46 @@
+<template>
+    <StackLayout>
+    <Label class="message" height="auto">Din reaktionstid: {{ time }}</Label>
+    <Label class="rank"> {{rank}} </Label>
+    </StackLayout>
+</template>
+
+<script>
+export default {
+  props: ['time'],
+  data() {
+    return {
+      rank: null,
+    }
+  },
+  mounted() {
+    if (this.time < 170 ) {
+      this.rank = 'Chuck Norris!'
+    } else if (this.time < 250) {
+      this.rank = 'Snabb som djävulen!'
+    } else if (this.time < 380) {
+      this.rank = 'Snabbare kan du.'
+    } else {
+      this.rank = 'Min mormor kan bättre...'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .rank {
+    font-size: 20;
+    font-weight: bold;
+    color: #e65a33;
+    text-align: center;
+    padding: 20 0;
+  }
+
+  .message {
+      vertical-align: center;
+      text-align: center;
+      font-size: 20;
+      color: #333333;
+      padding: 20 0;
+  }
+</style>
