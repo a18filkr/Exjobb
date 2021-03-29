827b2af758209ed35486d3f4fa0175bb058619fa
a18filkr <a18filkr@student.his.se>
2021-03-29 20:52:26 +0200

    Added filler-component to nativescript app

---
+++ pilot-nativescript/src/components/Filler.vue
@@ -0,0 +1,20 @@
+<template>
+  <p>{{ joke }}</p>
+</template>
+
+<script>
+export default {
+  props: ['joke'],
+}
+</script>
+
+<style scoped>
+  p {
+    background: #2c3e50;
+    color: #e65a33;
+    padding: 20px;
+    max-width: 400px;
+    margin: 5px auto;
+    border-radius: 5px;
+  }
+</style>
